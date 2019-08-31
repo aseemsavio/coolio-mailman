eeb7cf44ad95111d338f440ec82e2ad24a8a5c9a
Aseem Savio <aseemsavio3@gmail.com>
2019-09-01 01:17:28 +0530

    Initial commit

---
+++ src/main/java/com/coolio/mailman/CoolioMailmanApplication.java
@@ -0,0 +1,12 @@
+package com.coolio.mailman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolioMailmanApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolioMailmanApplication.class, args);
+    }
+}
